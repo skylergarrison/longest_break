7a4524170333c1e5e2a3f7814547ff0da2101387
Skyler Garrison <42485938+skylergarrison@users.noreply.github.com>
2019-04-08 22:29:01 -0700

    now prints a hall of fame of longest song gap

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import spotipy
 import spotipy.util as util
 import calendar
-from datetime import datetime
+from datetime import datetime, timedelta
 import itertools
 
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b = itertools.tee(iterable)
     next(b, None)
-
-    diff = b['added_at'] - a['added_at']
-    d = datetime(1,1,1) + diff
-    print_diff = "%d:%d:%d:%d" % (d.day-1, d.hour, d.minute, d.second)
-
-    print(b['name'] + '->' + a['name'] + '=' + print_diff)
+    return zip(a, b)
 
 username = '***REMOVED***'
 scope = 'playlist-read-collaborative'
@@ -40,4 +35,28 @@ for song in song_list:
     song['added_at'] = calendar.timegm(datetime_object.utctimetuple())
     print(song)
 
-pairwise(song_list)
+longest_time = 0
+offending_songs = ''
+hall_of_fame = []
+
+for i in pairwise(song_list):
+    print(i)
+    a = i[0]
+    b = i[1]
+
+    diff = b['added_at'] - a['added_at']
+    t = timedelta(seconds=diff)
+    print_diff = "Time to Keep: {}m-{}d {}".format(t.days/30, t.days%30, timedelta(seconds=t.seconds))
+    distance = a['track']['name'] + '->' + b['track']['name'] + '=' + print_diff
+    print(distance)
+
+    if a['track']['name']!=b['track']['name'] and diff > longest_time:
+        longest_time = diff
+        offending_songs = distance
+
+    hall_of_fame.append((distance, diff))
+
+hall_of_fame.sort(key=lambda tup: tup[1])
+
+for i in hall_of_fame:
+    print(i)
